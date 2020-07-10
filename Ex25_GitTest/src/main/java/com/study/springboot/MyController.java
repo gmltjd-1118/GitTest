cfedf77614dfae0f377dfde9bc9401b1634cf12c
user <user@i7C-55>
2020-07-10 10:08:29 +0900

    초기버전

---
+++ Ex25_GitTest/src/main/java/com/study/springboot/MyController.java
@@ -0,0 +1,21 @@
+package com.study.springboot;
+
+public class MyController {
+	
+	// Git Test
+	// Git 사용하는 장점
+	// 1. 소스 안전 관리
+	// 2. 히스토리 관리 가능 - 수정/추가/삭제/변경 기록이 다 남음
+	// 3. 팀 작업이 용이하다
+	
+	// clone(복제) : 서버저장소를 로컬에 복제
+	
+	// 프로젝트소스 폴더
+	//  |		   ^
+	// 	v commit   | fetch
+	// 로컬PC 저장소
+	//  |		   ^
+	// 	v push     | pull
+	// 원격 저장소(Github 서버)
+	
+}
